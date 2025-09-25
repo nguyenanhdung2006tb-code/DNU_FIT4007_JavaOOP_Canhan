53d4c621a93f5cdc6280c3ff686365a7b4b718a9
Nguyen Anh Dung <nguyenanhdung2006tb@gmail.com>
2025-09-25 16:27:43 +0700

    feat(part4): add input handling for Example15SumFormula

--- src/part4/loops/Example15SumFormula.java
+++ src/part4/loops/Example15SumFormula.java
@@ -1,7 +1,12 @@
 package part4.loops;
 
+import java.util.Scanner;
+
 public class Example15SumFormula {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Nhập số nguyên dương N: ");
+        int N = sc.nextInt();
 
     }
 }
